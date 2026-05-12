94ba41f96f3ead4c2677cc1a2d76f87ffe01bb78
graceboni <graceboni15@gmail.com>
2026-05-12 13:12:00 -0400

    added class

---
+++ src/main/java/org/example/House.java
@@ -0,0 +1,71 @@
+package org.example;
+
+public class House extends Asset{
+
+    private String address;
+    private int condition;
+    private int squareFoot;
+    private int lotSize;
+
+    public House(String description, String dateAcquired, double originalCost,
+                 String address, int condition, int squareFoot, int lotSize) {
+        super(description, dateAcquired, originalCost);
+
+        this.address = address;
+        this.condition = condition;
+        this.squareFoot = squareFoot;
+        this.lotSize = lotSize;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    public void setCondition(int condition) {
+        this.condition = condition;
+    }
+
+    public int getSquareFoot() {
+        return squareFoot;
+    }
+
+    public void setSquareFoot(int squareFoot) {
+        this.squareFoot = squareFoot;
+    }
+
+    public int getLotSize() {
+        return lotSize;
+    }
+
+    public void setLotSize(int lotSize) {
+        this.lotSize = lotSize;
+    }
+
+    @Override
+    public double getValue(){
+
+        double pricePerSquareFoot = 0;
+
+        if (condition == 1){
+            pricePerSquareFoot = 180;
+        }
+        else if (condition == 2){
+            pricePerSquareFoot = 130;
+        }
+        else if (condition == 3){
+            pricePerSquareFoot = 90;
+        }
+        else if (condition == 4){
+            pricePerSquareFoot = 80;
+        }
+        return (squareFoot * pricePerSquareFoot) + (lotSize * 0.25);
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,19 @@
 package org.example;
 
+import java.util.ArrayList;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<Asset> myAssets = new ArrayList<>();
+
+        myAssets.add(new House("My house","2026-03-20",15000, "234 Main street",
+                1, 2000, 5000));
+
+        myAssets.add(new House("Vacation home.", "2025-10-22", 250000,
+                "1877 beach road", 2, 1500, 6000));
 
     }
 }
